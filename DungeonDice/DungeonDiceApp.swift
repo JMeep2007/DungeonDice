901d204ec35297ba298ff41117752f6614c03ee4
JMeep2007 <meepegamajulian1@gmail.com>
2026-02-05 12:39:38 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by MEEPEGAMA, JULIAN on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
